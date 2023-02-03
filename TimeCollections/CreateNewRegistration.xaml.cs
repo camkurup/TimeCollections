8d309fb119da2a50d6c08e0f98f34d148826c09e
JulieSøndergaard-Hilleke <jsh@itklyngen.dk>
2023-02-03 11:10:42 +0100

    New list

--- TimeCollections/CreateNewRegistration.xaml.cs
+++ TimeCollections/CreateNewRegistration.xaml.cs
@@ -4,6 +4,8 @@ namespace TimeCollections;
 
 public partial class CreateNewRegistration : ContentPage
 {
+
+	List<TimeRegistration> registrations = new List<TimeRegistration>();
 	public CreateNewRegistration()
 	{
 		InitializeComponent();
@@ -11,7 +13,30 @@ public partial class CreateNewRegistration : ContentPage
 
 	private void OnCreateRegistration(object sender, EventArgs e)
 	{
+		TimeRegistration timeRegistration = new TimeRegistration(
+			InputProjectName.ToString(), 
+			InputCustomer.ToString(), 
+			InputEmployee.ToString(), 
+			InputActivity.ToString(), 
+			InputText.ToString());
 
+
+		registrations.Add(timeRegistration);
+	}
+
+	private void AddNewRegistrations()
+	{
+		List<TimeRegistration> newTimeRegistrations = new List<TimeRegistration> {
+		new TimeRegistration("project1", "Hans Hansen ApS", "JSH", "Programmering", "PIM Project"),
+		new TimeRegistration("project2", "Malermester Søren & Søn AS", "JSH", "Konsulenttimer", "Kontogrupper"),
+		new TimeRegistration("project3", "Elektrikeren ApS", "JSH", "Konsulenttimer", "Business Robot"),
+		new TimeRegistration("project4", "VVSkovs ApS", "JSH", "Konsulenttimer", "Finansbudget"),
+		new TimeRegistration("project5", "Marius M AS", "JSH", "Programmering", "Open Economy"),
+		new TimeRegistration("project6", "LEMASH ApS", "JSH", "Programmering", "API"),
+		new TimeRegistration("project7", "Lolland-Falsters Bryghus AS", "JSH", "Konsulenttimer", "PowerBI")
+	};
+
+		registrations.AddRange(newTimeRegistrations);
 	}
 
 
--- TimeCollections/Model/TimeRegistration.cs
+++ TimeCollections/Model/TimeRegistration.cs
@@ -14,7 +14,7 @@ namespace TimeCollections.Model
 		private string activity;
 		private string text = "";
 		private string time = "00:00:00";
-
+		
 		public string Project { get { return project; } set { project = value; } }
 		public string Customer { get { return customer; } set { customer = value; } }
 		public string Employee { get { return employee; } set { employee = value; } }
@@ -24,25 +24,11 @@ namespace TimeCollections.Model
 
 		public TimeRegistration(string _project, string _customer, string _employee, string _activity, string _text)
 		{
-
 			this.project = _project;
 			this.customer = _customer;
 			this.employee = _employee;
 			this.activity = _activity;
 			this.text = _text;
-		}
-
-		public void CreateTestProjects()
-		{
-			List<TimeRegistration> timeRegistrations = new List<TimeRegistration> {
-		new TimeRegistration("project1", "Hans Hansen ApS", "JSH", "Programmering", "PIM Project"),
-		new TimeRegistration("project2", "Malermester Søren & Søn AS", "JSH", "Konsulenttimer", "Kontogrupper"),
-		new TimeRegistration("project3", "Elektrikeren ApS", "JSH", "Konsulenttimer", "Business Robot"),
-		new TimeRegistration("project4", "VVSkovs ApS", "JSH", "Konsulenttimer", "Finansbudget"),
-		new TimeRegistration("project5", "Marius M AS", "JSH", "Programmering", "Open Economy"),
-		new TimeRegistration("project6", "LEMASH ApS", "JSH", "Programmering", "API"),
-		new TimeRegistration("project7", "Lolland-Falsters Bryghus AS", "JSH", "Konsulenttimer", "PowerBI")
-	};
 		}
 
 	}
