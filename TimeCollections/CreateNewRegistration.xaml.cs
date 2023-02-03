da24ab092df0e5847599360435c07a6411fdc19c
JulieSøndergaard-Hilleke <jsh@itklyngen.dk>
2023-02-03 11:13:12 +0100

    en anden mulighed

--- TimeCollections/CreateNewRegistration.xaml.cs
+++ TimeCollections/CreateNewRegistration.xaml.cs
@@ -5,7 +5,15 @@ namespace TimeCollections;
 public partial class CreateNewRegistration : ContentPage
 {
 
-	List<TimeRegistration> registrations = new List<TimeRegistration>();
+	List<TimeRegistration> registrations = new List<TimeRegistration> {
+		new TimeRegistration("project1", "Hans Hansen ApS", "JSH", "Programmering", "PIM Project"),
+		new TimeRegistration("project2", "Malermester Søren & Søn AS", "JSH", "Konsulenttimer", "Kontogrupper"),
+		new TimeRegistration("project3", "Elektrikeren ApS", "JSH", "Konsulenttimer", "Business Robot"),
+		new TimeRegistration("project4", "VVSkovs ApS", "JSH", "Konsulenttimer", "Finansbudget"),
+		new TimeRegistration("project5", "Marius M AS", "JSH", "Programmering", "Open Economy"),
+		new TimeRegistration("project6", "LEMASH ApS", "JSH", "Programmering", "API"),
+		new TimeRegistration("project7", "Lolland-Falsters Bryghus AS", "JSH", "Konsulenttimer", "PowerBI")
+	};
 	public CreateNewRegistration()
 	{
 		InitializeComponent();
