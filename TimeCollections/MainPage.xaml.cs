58c1c13ce635dd59f3a51d1cde0fd51c5dc24ce9
JulieSøndergaard-Hilleke <jsh@itklyngen.dk>
2023-02-03 08:47:35 +0100

    logic regarding timer, and TimeRegistration class

--- TimeCollections/MainPage.xaml.cs
+++ TimeCollections/MainPage.xaml.cs
@@ -1,24 +1,81 @@
-﻿namespace TimeCollections;
+﻿using System.Data;
+using System.Diagnostics;
+using System.Timers;
+using System.Threading;
+
+
+
+
+namespace TimeCollections;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	private IDispatcherTimer timer;
+
+	private int counterInSeconds;
+
+	private bool isRunning;
+
+
+
 
 	public MainPage()
 	{
 		InitializeComponent();
+
+		timer = Dispatcher.CreateTimer();
+		timer.Interval = TimeSpan.FromSeconds(1);
+		timer.Tick += Timer_Tick;
+
+
+		Button startButton = new Button { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Start" };
+		startButton.Clicked += StartButton_Clicked;
+		Button stopButton = new Button { VerticalOptions = LayoutOptions.Center, HorizontalOptions = LayoutOptions.Center, Text = "Stop" };
+		stopButton.Clicked += StopButton_Clicked;
+
+		Content = new StackLayout
+		{
+			Children =
+			{
+				counterLabel,
+				startButton,
+				stopButton
+			}
+		};
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void Timer_Tick(object sender, EventArgs e)
 	{
-		count++;
+		counterInSeconds++;
+		int minutes = counterInSeconds / 60;
+		int seconds = counterInSeconds % 60;
+		int hours = minutes / 60;
+		minutes = minutes % 60;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		counterLabel.Text = $"{hours:D2}:{minutes:D2}:{seconds:D2}";
 	}
+
+	public void StartButton_Clicked(object sender, EventArgs e)
+	{
+		
+
+		timer.Start();
+	
+		isRunning = true;
+
+	}
+	
+	public void StopButton_Clicked(object sender, EventArgs e)
+	{
+		
+
+		timer.Stop();
+	
+		isRunning = false;
+
+	}
+
+	
+
 }
 
---
+++ TimeCollections/Model/TimeRegistration.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeCollections.Model
+{
+	class TimeRegistration
+	{
+		private string project;
+		private string customer;
+		private string employee;
+		private string activity;
+		private string text = "";
+		private string time = "00:00:00";
+
+		public string Project { get { return project; } set { project = value; } }
+		public string Customer { get { return customer; } set { customer = value; } }
+		public string Employee { get { return employee; } set { employee = value; } }
+		public string Activity { get { return activity; } set { activity = value; } }
+		public string Text { get { return text; } set { text = value; } }
+		public string Time { get { return time; } set { time = value; } }
+
+		public TimeRegistration(string _project, string _employee)
+		{
+
+			this.project = _project;
+			this.employee = _employee;
+		}
+
+	}
+}
